cb1ff05b42a248a50d6fef654b61440025e5ed49
Ichbinositu <hesbon.ezekiel@student.moringaschool.com>
2024-01-08 21:51:35 +0300

    Done with Assignment

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function(drivers){
+     return [drivers[0],drivers[1]];
+};
+
+const returnLastTwoDrivers = function(drivers){
+    let size = drivers.length;
+     return [drivers[size -2],drivers[size - 1]];
+};
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier(integer) {
+  return function(fare) {
+    return fare * integer;
+  };
+}
+
+const fareDoubler = function (fare ){
+    return fare * 2;
+}
+
+const fareTripler = function (fare ){
+    return fare * 3;
+}
+
+const selectDifferentDrivers = function (arrayOfDrivers, func ){
+    return func(arrayOfDrivers);
+}
